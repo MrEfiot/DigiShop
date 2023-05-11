23f66ccf21db4703ab10676b30fee6128144215e
MrEfiot <mr.efiot@gmail.com>
2023-05-11 19:33:31 +0330

    add favicon - create go template add bootstrap for product upload

--- server/handler/view_handlers.go
+++ server/handler/view_handlers.go
@@ -7,10 +7,13 @@ import (
 
 func ViewMainHandler(c *gin.Context) {
 	c.HTML(http.StatusOK, "index.gohtml", gin.H{
+		"title":   "DigiShop",
 		"welcome": "Welcome to DigiShop",
 	})
 }
 
 func ViewProductUploadHandler(c *gin.Context) {
-	c.HTML(http.StatusOK, "upload_product.gohtml", gin.H{})
+	c.HTML(http.StatusOK, "product_upload.gohtml", gin.H{
+		"title": "Upload Product",
+	})
 }
