e1e50a49f2456e21146205241c09b8ddbd1676fc
MrEfiot <mr.efiot@gmail.com>
2023-05-12 22:04:14 +0330

    create jwt token and set cookie token in client user

--- server/handler/auth_handler.go
+++ server/handler/auth_handler.go
@@ -4,10 +4,13 @@ import (
 	"DigiShop/database"
 	"DigiShop/database/controller"
 	"DigiShop/database/models"
+	"DigiShop/tools"
 	"errors"
+	"github.com/dgrijalva/jwt-go"
 	"github.com/gin-gonic/gin"
 	"gorm.io/gorm"
 	"net/http"
+	"time"
 )
 
 func AuthHandler(c *gin.Context) {
@@ -23,9 +26,37 @@ func AuthHandler(c *gin.Context) {
 
 	success := controller.CompareHash(user.Password, password)
 	if success {
-		c.String(http.StatusOK, "your logged in :)")
+		token, expireTime := createToken(user.ID)
+		setCookieToken(token, expireTime, c)
+		c.String(http.StatusOK, "Logged in successfully")
 	} else {
 		c.String(http.StatusOK, "password is wrong!")
 		return
 	}
+
+}
+
+func createToken(userID uint) (string, time.Time) {
+	token := jwt.New(jwt.SigningMethodHS256)
+	claims := token.Claims.(jwt.MapClaims)
+
+	claims["user_id"] = userID
+
+	expirationTime := time.Now().Add(24 * time.Hour)
+	claims["exp"] = expirationTime.Unix()
+
+	tokenString, err := token.SignedString([]byte("DigiShop_KEY"))
+	tools.CheckError(err, "failed to generate jwt token")
+
+	return tokenString, expirationTime
+}
+
+func setCookieToken(tokenString string, expireTime time.Time, c *gin.Context) {
+	name := "token"
+	token := tokenString
+	expire := int(expireTime.Unix())
+	path := "/"
+	domain := ""
+
+	c.SetCookie(name, token, expire, path, domain, true, true)
 }
